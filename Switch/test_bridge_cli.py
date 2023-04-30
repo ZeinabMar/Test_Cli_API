748870bdf0d2a2e7d40087c7f4586786df90abc5
zeinabmaroufi <zeinab.maroufi@sinacomsys.com>
2023-04-30 05:58:46 -0700

    service senario1

--- Switch/test_bridge_cli.py
+++ Switch/test_bridge_cli.py
@@ -17,21 +17,21 @@ logger = logging.getLogger(__name__)
 Bridge = namedtuple('Bridge', ['bridgeId', 'bridgeProtocol', 'ageingTime', 'forwardTime', 'helloTime', 'maxAge',
                                'maxHops', 'priority', 'id', 'result'])
 Bridge.__new__.__defaults__ = (1, "IEEE_VLAN_BRIDGE", 100, 15, 2, 20, 20, 32768, 1, "Pass", 1, 1, None)
-BRIDGE_DATA = (
+BRIDGE_DATA = [
     Bridge(1, 'ieee', 100, 30, 1, 6, priority=4096),
-    Bridge(1, 'ieee_vlan_bridge', 1000, 29, 2, 7, priority=8192),
-    Bridge(1, 'nstp', 100, 30, maxAge=6, maxHops=1, priority=12288),
-    Bridge(1, 'mstpring', 1000, 29, maxAge=7, maxHops=20, priority=16384),
-    Bridge(1, 'rpvstp', 100, priority=20480),
-    Bridge(1, 'rstp', 1000, 29, 2, 7, priority=24576),
-    Bridge(1, 'rstp_ring', 100, 30, 1, 6, priority=28672),
-    Bridge(1, 'rstp_vlan_bridge', 1000, 29, 2, 7, priority=32768),
-    Bridge(1, 'rstp_vlan_bridge_ring', 100, 30, 1, 6, priority=36864),
-    Bridge(1, 'provider_mstp', 1000, 29, maxAge=7, maxHops=35, priority=40960),
-    Bridge(1, 'provider_mstp_edge', 100, 30, maxAge=6, maxHops=39, priority=45056),
-    Bridge(1, 'provider_rstp', 1000, 29, 2, 7, priority=49152),
-    Bridge(1, 'provider_rstp_edge', 1000, 29, 2, 7, priority=53248)
-)
+    # Bridge(1, 'ieee_vlan_bridge', 1000, 29, 2, 7, priority=8192),
+    # Bridge(1, 'nstp', 100, 30, maxAge=6, maxHops=1, priority=12288),
+    # Bridge(1, 'mstpring', 1000, 29, maxAge=7, maxHops=20, priority=16384),
+    # Bridge(1, 'rpvstp', 100, priority=20480),
+    # Bridge(1, 'rstp', 1000, 29, 2, 7, priority=24576),
+    # Bridge(1, 'rstp_ring', 100, 30, 1, 6, priority=28672),
+    # Bridge(1, 'rstp_vlan_bridge', 1000, 29, 2, 7, priority=32768),
+    # Bridge(1, 'rstp_vlan_bridge_ring', 100, 30, 1, 6, priority=36864),
+    # Bridge(1, 'provider_mstp', 1000, 29, maxAge=7, maxHops=35, priority=40960),
+    # Bridge(1, 'provider_mstp_edge', 100, 30, maxAge=6, maxHops=39, priority=45056),
+    # Bridge(1, 'provider_rstp', 1000, 29, 2, 7, priority=49152),
+    # Bridge(1, 'provider_rstp_edge', 1000, 29, 2, 7, priority=53248)
+]
 
 
 def bridge_cli(cli_interface_module, data=Bridge()):
@@ -56,9 +56,10 @@ def bridge_cli(cli_interface_module, data=Bridge()):
     for i in range(0,len(config_of_bridge)):
         check.is_not_in(config_of_bridge[i], result, msg= f" exist this config {config_of_bridge[i]}")  
 
-@pytest.mark.parametrize('data', BRIDGE_DATA)
+# @pytest.mark.parametrize('data', BRIDGE_DATA)
 def test_bridge_cli(cli_interface_module):
-    bridge_cli(cli_interface_module, data)
+    for data in BRIDGE_DATA:
+        bridge_cli(cli_interface_module, data)
 
 
     
