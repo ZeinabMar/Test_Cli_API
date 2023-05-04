bb1041d8d6050e7f94e499c48031a8d03c4b400b
zeinabmaroufi <zeinab.maroufi@sinacomsys.com>
2023-05-04 00:42:08 -0700

    service

--- config.py
+++ config.py
@@ -16,6 +16,23 @@ config_Senario_1 = {'total':["bridge 1 protocol ieee"],
                     ["max-frame 2550", 
                     "switchport", 
                     "bridge-group 1 spanning-tree enable stp"]}
+Unconfig_Senario_1 = {'total':["no bridge 1 protocol ieee"],
+                    'gponolt1':
+                    ["no bridge-group 1 spanning-tree enable",
+                     "no switchport",
+                     "no description",
+                     "no flowctrl tx",
+                     "no max-frame 1850"],
+                    'gponolt2':
+                    ["no bridge-group 1 spanning-tree disable",
+                    "no switchport",
+                    "no description",
+                    "no flowctrl rx",
+                    "no max-frame 1950"],
+                    'gponolt3':
+                    ["no bridge-group 1 spanning-tree enable stp",
+                    "no switchport", 
+                    "no max-frame 2550"]}                    
 
 config_check_Senario_1 = {'total':["bridge 1 protocol ieee"],
                             'gponolt1':
@@ -37,33 +54,61 @@ config_check_Senario_1 = {'total':["bridge 1 protocol ieee"],
 
 #**************************************************************************************
 config_Senario_2 = {'total':["bridge 1 protocol ieee", 
-                    "spanning-tree bridge 1 portfast bpdu-filter",
-                    "spanning-tree bridge 1 portfast bpdu-guard"],
+                        "spanning-tree bridge 1 portfast bpdu-filter",
+                        "spanning-tree bridge 1 portfast bpdu-guard"],
                     'ge1/1':
-                    ["speed 1G",
-                    "max-frame 1600",
-                    "switchport",
-                    "bridge-group 1 spanning-tree enable",
-                    "spanning-tree bpdu-filter enable",
-                    "spanning-tree bpdu-guard disable"
-                    ],
+                        ["speed 1G",
+                        "max-frame 1600",
+                        "switchport",
+                        "bridge-group 1 spanning-tree enable",
+                        "spanning-tree bpdu-filter enable",
+                        "spanning-tree bpdu-guard disable"
+                        ],
                     'ge1/2':[
-                    "speed 1G", 
-                    "max-frame 1700",
-                    "description",
-                    "switchport",
-                    "bridge-group 1 spanning-tree enable"
-                    "spanning-tree bpdu-filter disable",
-                    "spanning-tree bpdu-guard enable"],
+                        "speed 1G", 
+                        "max-frame 1700",
+                        "description",
+                        "switchport",
+                        "bridge-group 1 spanning-tree enable"
+                        "spanning-tree bpdu-filter disable",
+                        "spanning-tree bpdu-guard enable"],
                     'ge1/3':
-                    ["speed 10G", 
-                    "max-frame 1800", 
-                    "description",
-                    "switchport",
-                    "bridge-group 1 spanning-tree disable",
-                    "spanning-tree bpdu-filter enable",
-                    "spanning-tree bpdu-guard enable",
-                    "mirror both destination interface ge1/4"]}
+                        ["speed 10G", 
+                        "max-frame 1800", 
+                        "description",
+                        "switchport",
+                        "bridge-group 1 spanning-tree disable",
+                        "spanning-tree bpdu-filter enable",
+                        "spanning-tree bpdu-guard enable",
+                        "mirror both destination interface ge1/4"]}
+
+Unconfig_Senario_2 = {'total':["no spanning-tree bridge 1 portfast bpdu-guard",
+                        "no spanning-tree bridge 1 portfast bpdu-filter",
+                        "no bridge 1 protocol ieee"],
+                    'ge1/1':
+                    [   "no spanning-tree bpdu-guard disable",
+                        "no spanning-tree bpdu-filter enable",
+                        "no bridge-group 1 spanning-tree enable",
+                        "no switchport",
+                        "no max-frame 1600",
+                        "nospeed 1G"],
+                    'ge1/2':[
+                        "no spanning-tree bpdu-guard enable",
+                        "no spanning-tree bpdu-filter disable",
+                        "no bridge-group 1 spanning-tree enable"
+                        "no switchport",
+                        "no description",
+                        "no max-frame 1700",
+                        "no speed 1G"],
+                    'ge1/3':
+                    [   "no mirror both destination interface ge1/4",
+                        "no spanning-tree bpdu-guard enable",
+                        "no spanning-tree bpdu-filter enable",
+                        "no bridge-group 1 spanning-tree disable",
+                        "no switchport",
+                        "no description",
+                        "no max-frame 1800", 
+                        "no speed 10G"]}
 
 config_check_Senario_2 = {'total':["bridge 1 protocol ieee", 
                         "spanning-tree bridge 1 portfast bpdu-filter",
@@ -94,3 +139,49 @@ config_check_Senario_2 = {'total':["bridge 1 protocol ieee",
                         "spanning-tree bpdu-guard enable",
                         "mirror both destination interface ge1/4"]}                            
 #**************************************************************************************
+config_Senario_3 = {'total':["bridge 1 protocol ieee-vlan-bridge", 
+                        "spanning-tree bridge 1 portfast bpdu-filter",
+                        "spanning-tree bridge 1 portfast bpdu-guard"
+                        "vlan 10 bridge 1 type customer state enable",
+                        "vlan 11 bridge 1 type customer state enable",
+                        "vlan 12 bridge 1 type customer state enable",
+                        "vlan 13 bridge 1 type customer state enable",
+                        "vlan 14 bridge 1 type customer state enable"
+                        "vlan 15 bridge 1 type customer state enable"],
+                    'ge1/1':
+                        ["speed 1G",
+                        "max-frame 1600",
+                        "switchport",
+                        "bridge-group 1 spanning-tree enable",
+                        "switchport mode access",
+                        "switchport access 10",
+                        "spanning-tree bpdu-filter enable",
+                        "spanning-tree bpdu-guard enable",
+                        "storm-control dlf level 45",
+                        "storm-control broadcast level 55"
+                        ],
+                    'ge1/2':[
+                        "speed 1G", 
+                        "max-frame 1700",
+                        "description",
+                        "switchport",
+                        "bridge-group 1 spanning-tree enable"
+                        "switchport mode trunk",
+                        "switchport trunk tag 10-12",
+                        "no switchport trunk tag 11",
+                        "spanning-tree bpdu-filter enable",
+                        "spanning-tree bpdu-guard disable",
+                        "mirror ingress destination interface ge1/4"],
+                    'ge1/3':
+                        ["max-frame 1800", 
+                        "description",
+                        "switchport",
+                        "bridge-group 1 spanning-tree enable",
+                        "switchport mode trunk",
+                        "switchport trunk tag 11,13-15",
+                        "no switchport trunk tag 13",
+                        "spanning-tree bpdu-filter disable",
+                        "spanning-tree bpdu-guard enable",
+                        "storm-control broadcast level 65",
+                        "storm-control multicast level 45",
+                        "no storm-control multicast"]}
--- test_Service_Config.py
+++ test_Service_Config.py
@@ -8,16 +8,14 @@ import pytest_check as check
 from schema import Use
 from config import *
 
-Test_Target = 'PS6x_card'
 
-pytestmark = [pytest.mark.env_name("NP_env"), pytest.mark.cli_dev(Test_Target)]
+pytestmark = [pytest.mark.env_name("SNMP_CLI_env"), pytest.mark.cli_dev("snmp_cli")]
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def set_and_check_config_interface(cli_interface_module,config_array, check_array, index_port):
-
     command = "interface "+index_port
     cli_interface_module.exec(command)
     for i in range(0,len(config_array)):
@@ -26,28 +24,68 @@ def set_and_check_config_interface(cli_interface_module,config_array, check_arra
         check.is_in(check_array[i], result, msg= f"NOT EXIST THIS CONFIG {check_array[i]} IN INTERFACE {index_port}")
 
 def set_and_check_config_total(cli_interface_module,config_array, check_array):
-
     for i in range(0,len(config_array)):
         cli_interface_module.exec(f"{config_array[i]}")
         result = str(cli_interface_module.exec(f"show running-config | grep {config_array[i]}"))
         check.is_in(check_array[i], result, msg= f"not exist this config {check_array[i]} in interface total config")
-        
 
-def Senario_1(cli_interface_module, data_conf=config_Senario_1, data_check=config_check_Senario_1):
+def Unconfig_interface(cli_interface_module,Unconfig_array, index_port):
+    command = "interface "+index_port
+    cli_interface_module.exec(command)
+    for i in range(0,len(Unconfig_array)):
+        cli_interface_module.exec(f"{Unconfig_array[i]}")
+
+def Unconfig_total(cli_interface_module,Unconfig_array):
+    lenght = len(Unconfig_array)
+    for i in range(0,lenght):
+        if Unconfig_array[lenght-i].find("no ")!=-1 or Unconfig_array[lenght-i].find("disable")!=-1:
+            Unconfig_array[lenght-i].replace("no ", "")
+            Unconfig_array[lenght-i].replace("disable", "enable")
+            cli_interface_module.exec(f"{Unconfig_array[lenght-i]}")
+        else :
+            cli_interface_module.exec(f"no {Unconfig_array[lenght-i]}")
+
+
+def Senario_1(cli_interface_module, data_conf=config_Senario_1, data_check=config_check_Senario_1, data_unconfig=Unconfig_Senario_1):
     cli_interface_module.change_to_config()   
     set_and_check_config_total(cli_interface_module, data_conf['total'], data_check['total'])
     set_and_check_config_interface(cli_interface_module, data_conf['gponolt1'], data_check['gponolt1'], "gpon-olt1/1") 
     set_and_check_config_interface(cli_interface_module, data_conf['gponolt2'], data_check['gponolt2'], "gpon-olt1/2")    
-    set_and_check_config_interface(cli_interface_module, data_conf['gponolt3'], data_check['gponolt3'], "gpon-olt1/3")       
+    set_and_check_config_interface(cli_interface_module, data_conf['gponolt3'], data_check['gponolt3'], "gpon-olt1/3")  
+    cli_interface_module.exec("exit")
+    Unconfig_interface(cli_interface_module, data_conf['gponolt3'], "gpon-olt1/3")
+    Unconfig_interface(cli_interface_module, data_conf['gponolt2'], "gpon-olt1/2")
+    Unconfig_interface(cli_interface_module, data_conf['gponolt1'], "gpon-olt1/1")
+    Unconfig_total(cli_interface_module, data_unconfig['total'])
+
 
 def Senario_2(cli_interface_module, data_conf=config_Senario_2, data_check=config_check_Senario_2):
-    # cli_interface_module.change_to_config()   
-    # set_and_check_config_total(cli_interface_module, data_conf['total'], data_check['total'])
+    cli_interface_module.change_to_config()   
+    set_and_check_config_total(cli_interface_module, data_conf['total'], data_check['total'])
     set_and_check_config_interface(cli_interface_module, data_conf['ge1/1'], data_check['ge1/1'], "ge1/1") 
-    # set_and_check_config_interface(cli_interface_module, data_conf['ge1/2'], data_check['ge1/2'], "ge1/2")    
-    # set_and_check_config_interface(cli_interface_module, data_conf['ge1/3'], data_check['ge1/3'], "ge1/3")       
+    set_and_check_config_interface(cli_interface_module, data_conf['ge1/2'], data_check['ge1/2'], "ge1/2")    
+    set_and_check_config_interface(cli_interface_module, data_conf['ge1/3'], data_check['ge1/3'], "ge1/3")    
+    cli_interface_module.exec("exit")
+    Unconfig_interface(cli_interface_module, data_unconfig['ge1/3'], "ge1/3")
+    Unconfig_interface(cli_interface_module, data_unconfig['ge1/2'], "ge1/2")
+    Unconfig_interface(cli_interface_module, data_unconfig['ge1/1'], "ge1/1")
+    Unconfig_total(cli_interface_module, data_unconfig['total'])
+
+
+# def Senario_3(cli_interface_module, data_conf=config_Senario_3, data_check=config_check_Senario_3):
+#     cli_interface_module.change_to_config()   
+#     set_and_check_config_total(cli_interface_module, data_conf['total'], data_check['total'])
+#     set_and_check_config_interface(cli_interface_module, data_conf['ge1/1'], data_check['ge1/1'], "ge1/1") 
+#     set_and_check_config_interface(cli_interface_module, data_conf['ge1/2'], data_check['ge1/2'], "ge1/2")    
+#     set_and_check_config_interface(cli_interface_module, data_conf['ge1/3'], data_check['ge1/3'], "ge1/3")    
+#     cli_interface_module.exec("exit")
+#     Unconfig_interface(cli_interface_module, data_unconfig['ge1/3'], "ge1/3")
+#     Unconfig_interface(cli_interface_module, data_unconfig['ge1/2'], "ge1/2")
+#     Unconfig_interface(cli_interface_module, data_unconfig['ge1/1'], "ge1/1")
+#     Unconfig_total(cli_interface_module, data_unconfig['total'])
+      
 
 # @pytest.mark.parametrize('data', BRIDGE_DATA)
 def test_Senario(cli_interface_module):
-    # Senario_1(cli_interface_module, config_Senario_1, config_check_Senario_1)
-    Senario_2(cli_interface_module, config_Senario_2, config_check_Senario_2)
+    Senario_1(cli_interface_module, config_Senario_1, config_check_Senario_1, Unconfig_Senario_1)
+    # Senario_2(cli_interface_module, config_Senario_2, config_check_Senario_2, Unconfig_Senario_2)
