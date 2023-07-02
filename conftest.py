414fa56a1a64df80ec32a2255fe438c0e9156c89
zeinabmaroufi <zeinab.maroufi@sinacomsys.com>
2023-07-02 00:56:21 -0700

    update

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from collections import namedtuple
 import logging
 # Data:{PS6x_CARD, SC_CARD}
-Test_Target = 'snmp_cli'
+Test_Target = 'shelf_olt'
 # Data:{PS6x, PS4x}
 card_type = "sp5100"
 
--- data/envs/SNMP_CLI_env.py
+++ data/envs/SNMP_CLI_env.py
@@ -1,18 +1,24 @@
-DICT__SNMP__CLI = {
-    "index": None,
-    "snmp_ip": "192.168.9.127",
-    "snmp_community": "sina_private",
-    "snmp_version": '2',
-    "ssh_ip": "192.168.9.127",
-    "ssh_username": "root",
-    "ssh_password": "sina",
-    "cli_exec_username": "admin",
+
+DICT__SHELF = {
+    'type': "Shelf_OLT",
+    'node_id': None,
+    'shelf_id': "int",#1
+    'slot_id': "int",#1
+    'index': "int",
+    'snmp_ip': "192.168.9.127",
+    'snmp_community': "sina_private",
+    'snmp_version': "2",
+    'ssh_ip':"192.168.9.127",
+    'ssh_password': "sina",
+    'ssh_username': "root",
+    'cli_exec_username': "admin",
     "cli_exec_password": "admin",
-    "cli_enable_password": None,
-    "cli_config_password": None,
-    "cli_debug_password": None,
-    }
+    'cli_enable_password': None,
+    'cli_config_password': None,
+    'cli_debug_password': None
+}
 
 DICT__ENV = {
-    "snmp_cli": DICT__SNMP__CLI
-    }
+    'shelf_olt': DICT__SHELF
+}
+
